eb6e5eb213bac5d40b4911a111a6619a1c6f740f
Abdilahi1 <57772094+Abdilahi1@users.noreply.github.com>
2021-01-01 15:59:07 -0500

    update

--- assets/js/transitions.js
+++ assets/js/transitions.js
@@ -1,15 +1,20 @@
-var image = document.getElementById("menuButton");
-var sidebar = document.getElementById("side-bar");
-var container = document.getElementById("menuDiv")
+var btn = document.getElementById("btnNav");
+var sidebar = document.getElementById("sideBar");
+var container = document.getElementById("btnNavDiv")
+var ul = document.getElementById("sideBarList");
+var top = document.getElementById("test");
+var items = document.getElementsByClassName("hidden");
 
-image.addEventListener("click", function() {
+btn.addEventListener("click", function() {
 
 	if (sidebar.style.cssText === "") {
-		sidebar.style.cssText = "width: 15%;";
-		container.style.cssText = "margin-left: 15%;";
+		sidebar.style.cssText = "width: 20%;";
+		container.style.cssText = "margin-left: 20%;";
+		ul.style.cssText = "display: block;";
 	} else {
 		sidebar.style.cssText = "";
 		container.style.cssText = "";
+		ul.style.cssText = "";
 	}
 
 });
